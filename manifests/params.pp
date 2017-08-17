4953f831e85e088e729b584ac69156a95c12b19e
nikita tarasov <nikita@mygento.ru>
2017-08-17 13:39:18 +0300

    remove de

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,11 @@
 # Class  percona::params
 class percona::params {
-    $version = '5.6'
+    $version = '5.7'
     $root_password = undef
     $mysqltuner = true
     $service_ensure = true
     $service_enable = true
-    $pkg_common_default = ['percona-toolkit','libmysqlclient-dev']
+    $pkg_common_default = ['percona-toolkit']
     $remove_default_accounts = true
 
     $database_config = {}
