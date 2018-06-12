bd170d5a5e855aea4051f18aed9522459ccf2764
Nikita Tarasov <nikita@mygento.ru>
2018-06-12 15:07:30 +0300

    service_name

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,6 +3,7 @@ class percona::params {
     $version = '5.7'
     $root_password = undef
     $mysqltuner = true
+    $service_name = 'mysql'
     $service_ensure = true
     $service_enable = true
     $pkg_common_default = ['percona-toolkit']
